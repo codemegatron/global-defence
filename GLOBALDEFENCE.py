340f0a495ea6fa48560bc14ecab38056576867a8
codemegatron <fredifisher@outlook.com>
2018-03-10 16:04:09 +0000

    2nd commit

--- GLOBALDEFENCE.py
+++ GLOBALDEFENCE.py
@@ -1,4 +1,6 @@
 print("GDN sign in")
 password = CODEMEGATRON
-iors = input(PASSWORD FOR GLOBAL DEFENCE NETWORK-->)
+iors = input(PASSWORD FOR GLOBAL DEFENCE NETWORK-->).upper()
+if iors == password:
+    print("correct password")
 
