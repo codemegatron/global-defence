9af4e7f65f34d7f440ba2fb2a2b52ba5dd821520
codemegatron <fredifisher@outlook.com>
2018-03-10 16:11:11 +0000

    new code(3rd stage)

--- GLOBALDEFENCE.py
+++ GLOBALDEFENCE.py
@@ -2,5 +2,7 @@ print("GDN sign in")
 password = CODEMEGATRON
 iors = input(PASSWORD FOR GLOBAL DEFENCE NETWORK-->).upper()
 if iors == password:
-    print("correct password")
+	print("correct password")
+else:
+	print("incorrect password. to try again, run the program again.")
 
